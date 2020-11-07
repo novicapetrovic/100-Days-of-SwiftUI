bfcdbd5e2cd5a1e45c605d4d6b381667535095db
Novica Petrovic <novica.petrovic@clearscore.com>
2020-11-07 18:49:08 +0000

    Project 2: Challenge 3

--- NP - 100 Days of SwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ NP - 100 Days of SwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -59,7 +59,7 @@ struct ContentView: View {
             scoreTitle = "Correct"
             userScore += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong, that's the flag of \(countries[number])"
         }
         
         showingScore = true
